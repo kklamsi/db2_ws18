841a406b76499d3b54023dd26374321e91eff602
kklamsi <kl.klamsi@gmx.at>
2018-11-01 10:31:24 +0100

    First Commit

---
+++ pa0_klemens/src/pa0_klemens/Main.java
@@ -0,0 +1,14 @@
+package pa0_klemens;
+
+public class Main {
+	  public static void main(String[] args) {
+	    // TODO:
+	    // (1) check length of command-line arguments
+		  String[] stdout = args;
+		  int length = stdout.length;
+		  
+	    // (2) print the first command-line argument to stdout
+	 	  System.out.println(stdout[0]);
+	 	 
+	  }
+	}
